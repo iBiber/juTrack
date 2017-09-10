be76144a9bb0a6bb524b971fc78be9d6d9fd46a4
iBiber <iBiber@outlook.de>
2017-09-10 23:49:58 +0200

    Simply write result log into a text area

--- src/main/java/com/github/ibiber/jutrack/GetIssueResultItemPresenter.java
+++ src/main/java/com/github/ibiber/jutrack/GetIssueResultItemPresenter.java
@@ -2,19 +2,33 @@ package com.github.ibiber.jutrack;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.github.ibiber.jutrack.data.GetIssueResultItem;
 import com.github.ibiber.jutrack.data.GetIssuesParmeter;
+import com.github.ibiber.jutrack.ui.GetIssuesParameterDialog;
 
 @Component
 public class GetIssueResultItemPresenter {
+	@Autowired
+	private GetIssuesParameterDialog parameterDialog;
+
 	public void presentResults(GetIssuesParmeter parameter, Stream<GetIssueResultItem> resultStream) {
-		resultStream.forEach(item -> System.out
-		        .println(item.getDateTime().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) + "\t"
+		//		// Print to Console
+		//		resultStream.forEach(item -> System.out
+		//		        .println(item.getDateTime().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) + "\t"
+		//		                + item.key + "\t" + item.changeAction + "\t" + item.summary + "\t" + parameter.jiraRootUrl
+		//		                + "/browse/" + item.key));
+
+		// Show result to GUI
+		parameterDialog.setResultText(resultStream//
+		        .map(item -> item.getDateTime().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) + "\t"
 		                + item.key + "\t" + item.changeAction + "\t" + item.summary + "\t" + parameter.jiraRootUrl
-		                + "/browse/" + item.key));
+		                + "/browse/" + item.key)//
+		        .collect(Collectors.joining("\n")));
 	}
 }
--- src/main/java/com/github/ibiber/jutrack/ui/GetIssuesParameterDialog.java
+++ src/main/java/com/github/ibiber/jutrack/ui/GetIssuesParameterDialog.java
@@ -14,20 +14,24 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 @Component
 public class GetIssuesParameterDialog extends GridPane {
+	private TextArea resultArea;
+
 	@Autowired
-	public GetIssuesParameterDialog(Environment env,
-			JiraIssuesProcessor callback) {
+	public GetIssuesParameterDialog(Environment env, JiraIssuesProcessor callback) {
 		super();
 
 		GridPane gridpane = this;
@@ -58,20 +62,16 @@ public class GetIssuesParameterDialog extends GridPane {
 		gridpane.add(new Label("Jira password: "), 0, row);
 		gridpane.add(passwordField, 1, row++);
 
-		int dayRange = env.getProperty("ui.startDate.default.day.range",
-				Integer.class, 1);
-		DatePicker startDatePicker = new DatePicker(
-				LocalDate.now().minusDays(dayRange));
+		int dayRange = env.getProperty("ui.startDate.default.day.range", Integer.class, 1);
+		DatePicker startDatePicker = new DatePicker(LocalDate.now().minusDays(dayRange));
 		startDatePicker.getEditor().focusedProperty()
-				.addListener((obs, old, isFocused) -> updateDatePickerStructure(
-						startDatePicker, obs, old, isFocused));
+		        .addListener((obs, old, isFocused) -> updateDatePickerStructure(startDatePicker, obs, old, isFocused));
 		gridpane.add(new Label("Start date"), 0, row);
 		gridpane.add(startDatePicker, 1, row++);
 
 		DatePicker endDatePicker = new DatePicker(LocalDate.now());
 		endDatePicker.getEditor().focusedProperty()
-				.addListener((obs, old, isFocused) -> updateDatePickerStructure(
-						startDatePicker, obs, old, isFocused));
+		        .addListener((obs, old, isFocused) -> updateDatePickerStructure(startDatePicker, obs, old, isFocused));
 		gridpane.add(new Label("End date"), 0, row);
 		gridpane.add(endDatePicker, 1, row++);
 
@@ -79,42 +79,48 @@ public class GetIssuesParameterDialog extends GridPane {
 		startAction.defaultButtonProperty();
 		startAction.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				if (jiraRootUrlField.getText() == null
-						|| jiraRootUrlField.getText().isEmpty()) {
+				if (jiraRootUrlField.getText() == null || jiraRootUrlField.getText().isEmpty()) {
 					Alert alert = new Alert(AlertType.ERROR);
 					alert.setTitle("Missing jira root URL");
 					alert.setHeaderText("The jira root URL is mandatory!");
-					alert.setContentText(
-							"A jira root URL could look like 'http://jira.your-domain.com'.\n"
-									+ "You can set the jira root URL either in the text field or"
-									+ "define a default setting in a 'config/application.properties' "
-									+ "file with property '"
-									+ jiraRootUrlProperty + "'.");
+					alert.setContentText("A jira root URL could look like 'http://jira.your-domain.com'.\n"
+					        + "You can set the jira root URL either in the text field or"
+					        + "define a default setting in a 'config/application.properties' " + "file with property '"
+					        + jiraRootUrlProperty + "'.");
 
 					alert.showAndWait();
 				} else {
-					GetIssuesParmeter parameter = new GetIssuesParmeter(
-							jiraRootUrlField.getText(), userNameField.getText(),
-							passwordField.getText(), startDatePicker.getValue(),
-							endDatePicker.getValue());
+					GetIssuesParmeter parameter = new GetIssuesParmeter(jiraRootUrlField.getText(),
+					        userNameField.getText(), passwordField.getText(), startDatePicker.getValue(),
+					        endDatePicker.getValue());
 					callback.getIssues(parameter);
 				}
 			}
 		});
 		gridpane.add(startAction, 1, row++);
-
+		GridPane.setValignment(startAction, VPos.TOP);
 		GridPane.setHalignment(startAction, HPos.RIGHT);
+
+		resultArea = new TextArea();
+		resultArea.setPrefSize(600, 400);
+		resultArea.setMaxWidth(Double.MAX_VALUE);
+		resultArea.setMaxHeight(Double.MAX_VALUE);
+		gridpane.add(resultArea, 2, 0, 1, row);
+		GridPane.setHgrow(resultArea, Priority.ALWAYS);
+		GridPane.setVgrow(resultArea, Priority.ALWAYS);
 	}
 
-	private void updateDatePickerStructure(DatePicker datePicker,
-			ObservableValue<? extends Boolean> observable, Boolean oldValue,
-			Boolean isFocused) {
+	public void setResultText(String text) {
+		this.resultArea.setText(text);
+	}
+
+	private void updateDatePickerStructure(DatePicker datePicker, ObservableValue<? extends Boolean> observable,
+	        Boolean oldValue, Boolean isFocused) {
 		// This is an workaround for
 		// https://bugs.openjdk.java.net/browse/JDK-8136838
 		// (fixed with Java 8u72)
 		if (!isFocused) {
-			datePicker.setValue(datePicker.getConverter()
-					.fromString(datePicker.getEditor().getText()));
+			datePicker.setValue(datePicker.getConverter().fromString(datePicker.getEditor().getText()));
 		}
 	}
 }
