98597778ae0243e087ef66c4887093b06f260369
iBiber <iBiber@outlook.de>
2017-10-13 03:47:23 +0200

    Show the data in a table _Issue on the day_ #8

--- jutrack-gui/src/main/java/com/github/ibiber/jutrack/JuTrackGui.java
+++ jutrack-gui/src/main/java/com/github/ibiber/jutrack/JuTrackGui.java
@@ -1,10 +1,20 @@
 package com.github.ibiber.jutrack;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
@@ -13,6 +23,8 @@ import com.github.ibiber.jutrack.data.GetIssueResultItem;
 import com.github.ibiber.jutrack.data.GetIssuesParmeter;
 import com.github.ibiber.jutrack.util.Strings;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
@@ -24,6 +36,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -32,6 +48,7 @@ import javafx.scene.layout.Priority;
 @Component
 public class JuTrackGui extends GridPane implements GetIssueResultItemPresenter {
 	private TextArea resultArea;
+	private TableView table;
 
 	public void init(DefaultValueProvider defProperties, Consumer<GetIssuesParmeter> callback) {
 		GridPane gridpane = this;
@@ -92,13 +109,23 @@ public class JuTrackGui extends GridPane implements GetIssueResultItemPresenter 
 		GridPane.setValignment(startAction, VPos.TOP);
 		GridPane.setHalignment(startAction, HPos.RIGHT);
 
+		TabPane tabPane = new TabPane();
+		tabPane.setMaxWidth(Double.MAX_VALUE);
+		tabPane.setMaxHeight(Double.MAX_VALUE);
+		tabPane.setPrefSize(600, 400);
+
+		Tab tableTab = new Tab("Table View");
+		table = new TableView();
+		tableTab.setContent(table);
+
+		Tab textTab = new Tab("Text View");
 		resultArea = new TextArea();
-		resultArea.setPrefSize(600, 400);
-		resultArea.setMaxWidth(Double.MAX_VALUE);
-		resultArea.setMaxHeight(Double.MAX_VALUE);
-		gridpane.add(resultArea, 2, 0, 1, row);
-		GridPane.setHgrow(resultArea, Priority.ALWAYS);
-		GridPane.setVgrow(resultArea, Priority.ALWAYS);
+		textTab.setContent(resultArea);
+		tabPane.getTabs().addAll(tableTab, textTab);
+
+		gridpane.add(tabPane, 2, 0, 1, row);
+		GridPane.setHgrow(tabPane, Priority.ALWAYS);
+		GridPane.setVgrow(tabPane, Priority.ALWAYS);
 	}
 
 	private void updateDatePickerStructure(DatePicker datePicker, Boolean isFocused) {
@@ -113,10 +140,81 @@ public class JuTrackGui extends GridPane implements GetIssueResultItemPresenter 
 	@Override
 	public void presentResults(GetIssuesParmeter parameter, Stream<GetIssueResultItem> resultStream) {
 		// Show result to GUI
-		this.resultArea.setText(resultStream//
+		table.getColumns().clear();
+		createHeaderColumns(parameter);
+
+		// Fill table data and result text area
+		Rows rows = new Rows();
+		String resultText = resultStream//
+		        .peek(item -> rows.markAction(item.key, item.created)) // Collect data for table
 		        .map(item -> item.created.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) + "\t"
 		                + item.key + "\t" + item.changeAction + "\t" + item.summary + "\t" + parameter.jiraRootUrl
 		                + "/browse/" + item.key)//
-		        .collect(Collectors.joining("\n")));
+		        .collect(Collectors.joining("\n"));
+		this.resultArea.setText(resultText);
+
+		table.setItems(FXCollections.observableArrayList(rows.getRows()));
+	}
+
+	class Rows {
+		private Map<String, Row> rowsByIssueKey = new TreeMap<>();
+
+		public void markAction(String key, LocalDateTime dateTime) {
+			Row row = rowsByIssueKey.get(key);
+			if (row == null) {
+				row = new Row(key);
+				rowsByIssueKey.put(key, row);
+			}
+			row.markDate(dateTime);
+		}
+
+		public Collection<Row> getRows() {
+			return rowsByIssueKey.values();
+		}
+	}
+
+	class Row {
+		private final String issueKey;
+		private Map<String, String> columnValuesByColumnTitle = new HashMap<>();
+
+		public Row(String issueKey) {
+			this.issueKey = issueKey;
+		}
+
+		public void markDate(LocalDateTime dateTime) {
+			columnValuesByColumnTitle.put(getColumnHeader(dateTime), "X");
+		}
+
+		public String getIssueKey() {
+			return issueKey;
+		}
+
+		public String getValueByColumnTitle(String columnTitle) {
+			return columnValuesByColumnTitle.get(columnTitle);
+		}
+	}
+
+	private void createHeaderColumns(GetIssuesParmeter parameter) {
+		// Build first column
+		List<TableColumn> columns = new ArrayList<>();
+		TableColumn<Row, String> issuesColumn = new TableColumn<>("Issue");
+		issuesColumn.setCellValueFactory(row -> new ReadOnlyObjectWrapper<>(row.getValue().getIssueKey()));
+		columns.add(issuesColumn);
+
+		// Create one column per day
+		long daysBetween = ChronoUnit.DAYS.between(parameter.startDate, parameter.endDate);
+		IntStream.range(0, (int) daysBetween + 1).forEach(index -> {
+			LocalDate d = parameter.startDate.plusDays(index);
+			String columnTitle = getColumnHeader(d);
+			TableColumn<Row, String> col = new TableColumn<>(columnTitle);
+			col.setCellValueFactory(
+			        row -> new ReadOnlyObjectWrapper<>(row.getValue().getValueByColumnTitle(columnTitle)));
+			columns.add(col);
+		});
+		table.getColumns().addAll(columns);
+	}
+
+	private String getColumnHeader(Temporal dateTime) {
+		return DateTimeFormatter.ofPattern("dd.MM").format(dateTime);
 	}
 }
